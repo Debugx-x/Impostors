77ccc1744c10174544a4c4d7293e6f85b3192159
vaibhavs <vaibhavs@sfu.ca>
2020-10-20 14:28:05 -0700

    [vaibhav] Changes to game and options code

--- app/src/main/java/com/vaibhavs/MineSweeper/UI/game.java
+++ app/src/main/java/com/vaibhavs/MineSweeper/UI/game.java
@@ -25,11 +25,11 @@ public class game extends AppCompatActivity {
 
     Minesfield mf;
 
-    private int rows = 4;
-    private int col = 6;
+    private int rows;
+    private int col;
     private int mines_found = 0;
     private int Scan_no = 0;
-    Button[][] buttons = new Button[rows][col];
+    Button[][] buttons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +38,7 @@ public class game extends AppCompatActivity {
         mf = Minesfield.getInstance();
         rows = mf.getRows();
         col = mf.getCol();
-
+        buttons = new Button[rows][col];
         set_initialtexts();
         populateButtons();
         set_mines();
@@ -52,7 +52,7 @@ public class game extends AppCompatActivity {
             c = rand.nextInt(mf.getCol());
             if (buttons[r][c].getText() != "mine"){
                 buttons[r][c].setText("mine");
-                buttons[r][c].setTextColor(0xff0000);
+                buttons[r][c].setTextColor(0xffffff);
                 i++;
             }
         }
--- app/src/main/java/com/vaibhavs/MineSweeper/UI/option.java
+++ app/src/main/java/com/vaibhavs/MineSweeper/UI/option.java
@@ -6,6 +6,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -35,6 +36,8 @@ public class option extends AppCompatActivity {
         etcols = findViewById(R.id.input_col);
         etmines = findViewById(R.id.input_mines);
 
+        set_inputText();
+
         TextWatcher watcher = new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -53,10 +56,49 @@ public class option extends AppCompatActivity {
 
     }
 
+    private void set_inputText() {
+        etrows.setText("" + mf.getRows());
+        etcols.setText("" + mf.getCol());
+        etmines.setText("" + mf.getNo_of_mines());
+    }
+
     private void save_dimensions() {
-        mf.setRows(Integer.parseInt(etrows.getText().toString()));
-        mf.setCol(Integer.parseInt(etcols.getText().toString()));
-        mf.setNo_of_mines(Integer.parseInt(etmines.getText().toString()));
+        String rowStr = etrows.getText().toString();
+        String colStr = etcols.getText().toString();
+        String mineStr = etmines.getText().toString();
+
+        String msg = null;
+        try {
+            int row = Integer.parseInt(rowStr);
+            int col = Integer.parseInt(colStr);
+            int mine = Integer.parseInt(mineStr);
+
+            //Parameters checking
+            if(row <=3 ){
+                throw new IllegalArgumentException("No of rows cannot be less than 3");
+            } else if (row >=51){
+                throw new IllegalArgumentException("No of rows cannot be greater than 50");
+            }
+            if(col <= 5 ){
+                throw new IllegalArgumentException("No of columns cannot be less than 3");
+            } else if (col >=51){
+                throw new IllegalArgumentException("No of columns cannot be greater than 50");
+            }
+            if(mine <=6 ){
+                throw new IllegalArgumentException("Impostors cannot be less than 3");
+            } else if (row >=61){
+                throw new IllegalArgumentException("Impostors cannot be greater than 50");
+            }
+            // sets dimensions
+            mf.setRows(row);
+            mf.setCol(col);
+            mf.setNo_of_mines(mine);
+        } catch (IllegalArgumentException e){
+            msg = e.getMessage();
+        }
+        if (msg !=null){
+            Toast.makeText(option.this,msg,Toast.LENGTH_SHORT).show();
+        }
     }
 
     private void set_restbutton() {
